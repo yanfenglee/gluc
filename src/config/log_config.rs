2757f955753a9a17a66c50be8e834789c6559b7a
liyanfeng <muxsdt@gmail.com>
2021-08-23 13:52:44 +0800

    fix log init

--- src/config/log_config.rs
+++ src/config/log_config.rs
@@ -1,10 +1,19 @@
 
 use std::time::Duration;
 
-use fast_log::{consts::LogSize, plugin::file_split::RollingType};
+use fast_log::{consts::LogSize, plugin::{file_split::RollingType, packer::ZipPacker}};
 
 pub fn init_log() {
-    fast_log::init_split_log("target/logs/", 1000, LogSize::MB(1), false, RollingType::KeepTime(Duration::from_secs(3600*24)), log::Level::Info, None, true);
+    fast_log::init_split_log(
+        "target/logs/", 
+    1000, 
+    LogSize::MB(1), 
+    false, 
+            RollingType::KeepTime(Duration::from_secs(3600*24)), 
+            log::Level::Info, 
+    None, 
+    Box::new(ZipPacker{}), 
+    true);
 }
 
 
