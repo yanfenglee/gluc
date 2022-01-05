62c71ddf95ac39b0df8fc630108c067a0d1b0f52
liyanfeng <muxsdt@gmail.com>
2022-01-05 16:18:05 +0800

    update

--- src/config/log_config.rs
+++ src/config/log_config.rs
@@ -1,19 +1,18 @@
-
 use std::time::Duration;
 
-use fast_log::{consts::LogSize, plugin::{file_split::RollingType, packer::ZipPacker}};
+use fast_log::{
+    consts::LogSize,
+    plugin::{file_split::RollingType, packer::ZipPacker},
+};
 
 pub fn init_log() {
     fast_log::init_split_log(
-        "target/logs/", 
-    1000, 
-    LogSize::MB(1), 
-    false, 
-            RollingType::KeepTime(Duration::from_secs(3600*24)), 
-            log::Level::Info, 
-    None, 
-    Box::new(ZipPacker{}), 
-    true);
+        "target/logs/",
+        LogSize::MB(1),
+        RollingType::KeepTime(Duration::from_secs(3600 * 24)),
+        log::Level::Info,
+        None,
+        Box::new(ZipPacker {}),
+        true,
+    );
 }
-
-
